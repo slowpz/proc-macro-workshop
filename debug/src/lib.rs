87e60b37fc7cd42347920bb4a8dfe6c1fe9af682
pj_zhong <pj_zhong@163.com>
2023-07-07 16:06:13 +0800

    [debug] custom-format

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,58 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::{quote};
+use syn::{parse_macro_input, Data, DeriveInput, Error, Fields};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let data = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let stream = match debug_impl(&data) {
+        Ok(stream) => stream,
+        Err(err) => return err.into_compile_error().into(),
+    };
+
+    proc_macro::TokenStream::from(stream)
+}
+
+fn debug_impl(data: &DeriveInput) -> Result<TokenStream, Error> {
+    let ident = &data.ident;
+    let struct_data = if let Data::Struct(st) = &data.data {
+        st
+    } else {
+        return Err(syn::Error::new_spanned(ident, "Expect struct"));
+    };
+
+    let fields = match struct_data.fields {
+        Fields::Named(ref fields) => {
+            let recurse = fields.named.iter().map(|f| {
+                let ident = match f.ident.as_ref() {
+                    Some(ident) => ident,
+                    None => {
+                        return Error::new_spanned(ident, "anyonymous filed is not support")
+                            .into_compile_error()
+                    }
+                };
+
+                let ident_name = ident.to_string();
+
+                quote!(
+                    field(#ident_name, &self.#ident)
+                )
+            });
+
+            recurse
+        }
+        _ => return Err(Error::new_spanned(ident, "only support named field")),
+    };
+
+    let ident_name = ident.to_string();
+    Ok(quote! {
+        impl std::fmt::Debug for #ident {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                fmt.debug_struct(#ident_name)
+                #(.#fields)*
+                .finish()
+            }
+        }
+    })
 }
--- main.rs
+++ main.rs
@@ -1,30 +1,35 @@
-// Does your macro still work if some of the standard library prelude item names
-// mean something different in the caller's code?
+// Emit an implementation of std::fmt::Debug for a basic struct with named
+// fields and no generic type parameters.
 //
-// It may seem unreasonable to consider this case, but it does arise in
-// practice. Most commonly for Result, where crates sometimes use a Result type
-// alias with a single type parameter which assumes their crate's error type.
-// Such a type alias would break macro-generated code that expects Result to
-// have two type parameters. As another example, Hyper 0.10 used to define
-// hyper::Ok as a re-export of hyper::status::StatusCode::Ok which is totally
-// different from Result::Ok. This caused problems in code doing `use hyper::*`
-// together with macro-generated code referring to Ok.
+// Note that there is no enforced relationship between the name of a derive
+// macro and the trait that it implements. Here the macro is named CustomDebug
+// but the trait impls it generates are for Debug. As a convention, typically
+// derive macros implement a trait with the same name as a macro.
 //
-// Generally all macros (procedural as well as macro_rules) designed to be used
-// by other people should refer to every single thing in their expanded code
-// through an absolute path, such as std::result::Result.
+//
+// Resources:
+//
+//   - The Debug trait:
+//     https://doc.rust-lang.org/std/fmt/trait.Debug.html
+//
+//   - The DebugStruct helper for formatting structs correctly:
+//     https://doc.rust-lang.org/std/fmt/struct.DebugStruct.html
 
-use derive_builder::Builder;
+use derive_debug::CustomDebug;
 
-type Option = ();
-type Some = ();
-type None = ();
-type Result = ();
-type Box = ();
-
-#[derive(Builder)]
-pub struct Command {
-    executable: String,
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u8,
 }
 
-fn main() {}
+fn main() {
+    let f = Field {
+        name: "F",
+        bitmask: 0b00011100,
+    };
+
+    let debug = format!("{:?}", f);
+
+    assert!(debug.starts_with(r#"Field { name: "F","#));
+}
