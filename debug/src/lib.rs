222268fd369c8ac5b0ad640ee07652bb233b932f
pj_zhong <pj_zhong@163.com>
2023-07-07 17:53:13 +0800

    [debug] type-parameter

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,12 +1,13 @@
 use proc_macro2::TokenStream;
-use quote::{quote};
-use syn::{parse_macro_input, Data, DeriveInput, Error, Fields};
+use quote::quote;
+use std::result::Result;
+use syn::{parse_macro_input, Data, DeriveInput, Error, Fields, Generics, GenericParam, parse_quote};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let data = parse_macro_input!(input as DeriveInput);
 
-    let stream = match debug_impl(&data) {
+    let stream = match debug_impl(data) {
         Ok(stream) => stream,
         Err(err) => return err.into_compile_error().into(),
     };
@@ -14,7 +15,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::from(stream)
 }
 
-fn debug_impl(data: &DeriveInput) -> Result<TokenStream, Error> {
+fn debug_impl(data: DeriveInput) -> Result<TokenStream, Error> {
     let ident = &data.ident;
     let struct_data = if let Data::Struct(st) = &data.data {
         st
@@ -35,9 +36,15 @@ fn debug_impl(data: &DeriveInput) -> Result<TokenStream, Error> {
 
                 let ident_name = ident.to_string();
 
-                quote!(
-                    field(#ident_name, &self.#ident)
-                )
+                match debug_fmt(f) {
+                    Ok(Some(str)) => quote!(
+                        field(#ident_name, &std::format_args!(#str, &self.#ident))
+                    ),
+                    Ok(_) => quote!(
+                        field(#ident_name, &self.#ident)
+                    ),
+                    Err(e) => e.into_compile_error(),
+                }
             });
 
             recurse
@@ -45,9 +52,14 @@ fn debug_impl(data: &DeriveInput) -> Result<TokenStream, Error> {
         _ => return Err(Error::new_spanned(ident, "only support named field")),
     };
 
+    let generics = add_trait_bounds(data.generics);
+    let (impl_generics, ty_genrics, where_clause) = generics.split_for_impl();
+
+
+    
     let ident_name = ident.to_string();
     Ok(quote! {
-        impl std::fmt::Debug for #ident {
+        impl #impl_generics std::fmt::Debug for #ident #ty_genrics #where_clause {
             fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 fmt.debug_struct(#ident_name)
                 #(.#fields)*
@@ -55,4 +67,49 @@ fn debug_impl(data: &DeriveInput) -> Result<TokenStream, Error> {
             }
         }
     })
+}
+
+fn debug_fmt(f: &syn::Field) -> syn::Result<Option<String>> {
+    let name = None;
+    for attr in &f.attrs {
+        if !attr.path().is_ident("debug") {
+            continue;
+        }
+
+        if name.is_some() {
+            return Err(Error::new_spanned(&f.ident, "too much builder attrs"));
+        }
+
+        return match &attr.meta {
+            syn::Meta::NameValue(name_value) => match &name_value.value {
+                syn::Expr::Lit(lit) => match &lit.lit {
+                    syn::Lit::Str(str) => Ok(Some(str.value())),
+                    _ => Err(syn::Error::new_spanned(
+                        &attr.meta,
+                        "expected `debug = \"...\"`",
+                    )),
+                },
+                _ => Err(syn::Error::new_spanned(
+                    attr.meta.path().get_ident(),
+                    "Unknow expr",
+                )),
+            },
+            _ => Err(syn::Error::new_spanned(
+                attr.meta.path().get_ident(),
+                "Unknow meta",
+            )),
+        };
+    }
+
+    Ok(name)
+}
+
+// Add a bound `T: HeapSize` to every type parameter T.
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+    generics
 }
--- main.rs
+++ main.rs
@@ -1,35 +1,40 @@
-// Emit an implementation of std::fmt::Debug for a basic struct with named
-// fields and no generic type parameters.
+// Figure out what impl needs to be generated for the Debug impl of Field<T>.
+// This will involve adding a trait bound to the T type parameter of the
+// generated impl.
 //
-// Note that there is no enforced relationship between the name of a derive
-// macro and the trait that it implements. Here the macro is named CustomDebug
-// but the trait impls it generates are for Debug. As a convention, typically
-// derive macros implement a trait with the same name as a macro.
+// Callers should be free to instantiate Field<T> with a type parameter T which
+// does not implement Debug, but such a Field<T> will not fulfill the trait
+// bounds of the generated Debug impl and so will not be printable via Debug.
 //
 //
 // Resources:
 //
-//   - The Debug trait:
-//     https://doc.rust-lang.org/std/fmt/trait.Debug.html
+//   - Representation of generics in the Syn syntax tree:
+//     https://docs.rs/syn/1.0/syn/struct.Generics.html
 //
-//   - The DebugStruct helper for formatting structs correctly:
-//     https://doc.rust-lang.org/std/fmt/struct.DebugStruct.html
+//   - A helper for placing generics into an impl signature:
+//     https://docs.rs/syn/1.0/syn/struct.Generics.html#method.split_for_impl
+//
+//   - Example code from Syn which deals with type parameters:
+//     https://github.com/dtolnay/syn/tree/master/examples/heapsize
 
 use derive_debug::CustomDebug;
 
 #[derive(CustomDebug)]
-pub struct Field {
-    name: &'static str,
+pub struct Field<T> {
+    value: T,
+    #[debug = "0b{:08b}"]
     bitmask: u8,
 }
 
 fn main() {
     let f = Field {
-        name: "F",
+        value: "F",
         bitmask: 0b00011100,
     };
 
     let debug = format!("{:?}", f);
+    let expected = r#"Field { value: "F", bitmask: 0b00011100 }"#;
 
-    assert!(debug.starts_with(r#"Field { name: "F","#));
+    assert_eq!(debug, expected);
 }
