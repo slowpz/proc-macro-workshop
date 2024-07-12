33749ce1aded3a1dcbb230a4dd332d4a4dff64b1
pj_zhong <pj_zhong@163.com>
2024-07-12 15:56:52 +0800

    [seq] test2

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{braced, parse::Parse, parse_macro_input, DeriveInput, Token};
+use syn::{braced, parse::Parse, parse_macro_input, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
@@ -14,19 +14,27 @@ struct Seq {
     left: syn::LitInt,
     point_token: Token![..],
     right: syn::LitInt,
+    stream: proc_macro2::TokenStream,
 }
 
 impl Parse for Seq {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let seq = Self {
-            ident: input.parse()?,
-            in_token: input.parse()?,
-            left: input.parse()?,
-            point_token: input.parse()?,
-            right: input.parse()?,
-        };
-        let _content;
-        let _brace_token = braced!(_content in input);
-        Ok(seq)
+        let ident = input.parse()?;
+        let in_token = input.parse()?;
+        let left = input.parse()?;
+        let point_token = input.parse()?;
+        let right = input.parse()?;
+
+        let content;
+        let _ = braced!(content in input);
+        let stream = content.parse()?;
+        Ok(Self {
+            ident,
+            in_token,
+            left,
+            point_token,
+            right,
+            stream,
+        })
     }
 }
