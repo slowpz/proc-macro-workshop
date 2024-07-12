29e41bf0b840557dce810c5eecf6bc3da451ee5f
pj_zhong <pj_zhong@163.com>
2024-07-12 15:25:40 +0800

    [seq] test1

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+use syn::{braced, parse::Parse, parse_macro_input, DeriveInput, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    TokenStream::new()
+}
+
+struct Seq {
+    ident: syn::Ident,
+    in_token: Token![in],
+    left: syn::LitInt,
+    point_token: Token![..],
+    right: syn::LitInt,
+}
+
+impl Parse for Seq {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let seq = Self {
+            ident: input.parse()?,
+            in_token: input.parse()?,
+            left: input.parse()?,
+            point_token: input.parse()?,
+            right: input.parse()?,
+        };
+        let _content;
+        let _brace_token = braced!(_content in input);
+        Ok(seq)
+    }
 }
