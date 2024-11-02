01d75dc91e127998ee5fb6f274dd05d6c1c8396f
pj_zhong <pj_zhong@163.com>
2024-11-02 16:26:20 +0800

    [sorted] 1-4 finished

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,37 @@
-use proc_macro::TokenStream;
+use proc_macro::{Span, TokenStream};
+use syn::{parse_macro_input, spanned::Spanned, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let item = input.clone();
+    let item = parse_macro_input!(item as syn::Item);
+    let item = if let Item::Enum(item_enum) = item {
+        item_enum
+    } else {
+        return syn::Error::new(
+            Span::call_site().into(),
+            "expected enum or match expression",
+        )
+        .into_compile_error()
+        .into();
+    };
 
-    unimplemented!()
+    let idents = {
+        let mut idents = item.variants.iter().collect::<Vec<_>>();
+        idents.sort_by_key(|var| &var.ident);
+        idents
+    };
+    for (sorted, origin) in idents.iter().zip(item.variants.iter()) {
+        if sorted.ident != origin.ident {
+            return syn::Error::new(
+                sorted.span(),
+                format!("{} should sort before {}", sorted.ident, origin.ident),
+            )
+            .into_compile_error()
+            .into();
+        }
+    }
+
+    input
 }
